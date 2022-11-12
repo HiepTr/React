4c7d2e0c03f771f9597ff041ec2a4d6348ba1d50
HiepTr <fireorge@gmail.com>
2022-11-12 17:35:01 +0700

    final product

--- src/App.js
+++ src/App.js
@@ -1,51 +1,50 @@
-import  {useEffect } from 'react';
+import  {useState, useEffect } from 'react';
 import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from  './search.svg';
 const API_URL = 'http://www.omdbapi.com/?apikey=193381e9';
 
-const movie1 ={
-  
-    "Title": "Amazing Spiderman Syndrome",
-    "Year": "2012",
-    "imdbID": "tt2586634",
-    "Type": "movie",
-    "Poster": "N/A"
 
-}
 
 const App =() => {
-    
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
     const searchMovies = async (title) =>{
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data.Search)
+        setMovies(data.Search)
     };
     useEffect(()=>{searchMovies('Spiderman');},[]);
 
     return(
-        <div className="app">
-            <h1>MovieWorld</h1>
-            <div className ="search">
-                <input 
-                  value = ''
-                  onChange ={()=> {}}
-                  placeHolder ="Search"
-                />
-                <img
-                  src ={SearchIcon}
-                  alt =  "search"
-                  onClick ={()=> {}}
-                />
-            </div>
+      <div className="app">
+          <h1>MovieLand</h1>
+          <div className ="search">
+              <input 
+                value = {searchTerm}
+                onChange ={(e)=> setSearchTerm(e.target.value)}
+                placeHolder ="Type here to search movies"
+              />
+              <img
+                src ={SearchIcon}
+                alt =  "search"
+                onClick ={()=>searchMovies(searchTerm)}
+              />
+          </div>
+          {
+              movies?.length>0
+              ?(
+               <div className="container">
+                  {movies.map((movie)=>(<MovieCard movie ={movie}/>))}
+                  </div>   
+              ) : (
+                <div className= "empty">
+                  <h2>No movies found</h2>
+                </div>
+              )
+          }
 
-            <div className = "container">
-        
-                <MovieCard movie1 ={movie1} />
-               
-
-            </div>
   
           
             
