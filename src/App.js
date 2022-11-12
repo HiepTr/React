0ec5db85a40990a4ee7b411bb1ba5b4c4209b575
HiepTr <fireorge@gmail.com>
2022-11-12 16:34:57 +0700

    firt step with the website title + getting the URl to the movies database

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useEffect } from 'react';
+
+
+
+
+const API_URL = 'http://www.omdbapi.com/?apikey=193381e9';
+
 
 const App =() => {
+  
+    
+    
+    const searchMovies = async (title) =>{
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search)
+    };
+    useEffect(()=>{searchMovies('Spiderman');},[]);
+
     return(
-        <h1>App</h1>
+        <div className="app">
+            <h1>MovieWorld</h1>
+  
+          
+            
+        </div>
     );
-}
+};
 export default App;
